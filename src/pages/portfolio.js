c4f96f93fa534349f26846dec34d7f48e449d1d3
Ben Armstrong <benarmstrong@live.com>
2023-05-28 21:57:13 +0930

    image update

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -12,10 +12,10 @@ function Portfolio() {
       <img className="Project2" src={process.env.PUBLIC_URL + "/images/MetroMadness_Screen2_ShowingWork_450x223.png"} width= "580" alt="Project2"/></a>
 
       <a href="https://github.com/BenArmstrong81/Ben-Armstrong-Model-View-Controller-MVC-Tech-Blog">
-      <img className="MVC-TechBlog" src={process.env.PUBLIC_URL + "/images/Ben-Armstrong-Model-View-Controller-MVC-Tech-Blog_SignUpScreen.png"} width= "575" alt="MVC-TechBlog"/></a>
+      <img className="MVC-TechBlog" src={process.env.PUBLIC_URL + "/images/Ben-Armstrong-Model-View-Controller-MVC-Tech-Blog_SignUpScreen.png"} width= "575" alt="MVCTechBlog"/></a>
 
       <a href="https://github.com/BenArmstrong81/Ben-Armstrong-NoSQL-Social-Network-API">
-      <img className="NoSQL-Social-Network" src={process.env.PUBLIC_URL + "/images/AllThoughtsScreenShot.png"} width= "530" alt="NoSQL-Social-Network"/></a>
+      <img className="NoSQL-Social-Network" src={process.env.PUBLIC_URL + "/images/NoSQLSocialNetworkAPI.png"} width= "530" alt="NoSQLSocialNetwork"/></a>
 
       <a href="https://benarmstrong81.github.io/Ben-Armstrong-Web-APIs-Code-Quiz/">
       <img className="CodingQuiz-API" src={process.env.PUBLIC_URL + "/images/BenArmstrong-Web-API-Challenge-HomeScreen.png"} width= "580" alt="CodingQuiz-API"/></a>
